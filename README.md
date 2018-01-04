f9027eaf6e8998d6934dad1a4fdd0513d31a8d95
Charles <crossan007@gmail.com>
2018-01-03 21:24:55 -0500

    tweaks

---
+++ README.md
@@ -0,0 +1,16 @@
+#Sample Mail Server
+
+This repo is my baseline mail server configuration.
+
+Many of the configuration files here are sourced from the tutorial located at [https://www.exratione.com/2016/05/a-mailserver-on-ubuntu-16-04-postfix-dovecot-mysql/](https://www.exratione.com/2016/05/a-mailserver-on-ubuntu-16-04-postfix-dovecot-mysql/)
+
+
+# Usage
+
+## Run Sample server
+```vagrant up```
+
+## Generate config files
+Generates configuration files for referenced packages based on meta-config templates
+
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,31 @@
-sudo apt-get update 
-sudo apt-get -y dist-upgrade
+#!/usr/bin/env bash
+
+echo $(whoami)
+
+echo "=========================================================="
+echo "================  Update Base System  ===================="
+echo "=========================================================="
+sudo apt-get -qq update
+
+
+echo "=========================================================="
+echo "==========   Updating Vagrant Private Key     ============"
+echo "=========================================================="
+#
+echo "Creating Vagrant user"
+sudo useradd -m vagrant -s /bin/bash -d /home/vagrant
+sudo usermod -aG sudo vagrant
+echo -e "vagrant\nvagrant" | sudo passwd vagrant
+echo "vagrant ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
+
+echo "Updating SSH Key as per https://www.vagrantup.com/docs/boxes/base.html"
+sudo mkdir -p /home/vagrant/.ssh
+chmod 0700 /home/vagrant/.ssh
+wget --no-check-certificate \
+    https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub \
+    -O /home/vagrant/.ssh/authorized_keys
+    
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+sudo sed -i 's@^#AuthorizedKeysFile.*$@AuthorizedKeysFile %h/.ssh/authorized_keys@g' /etc/ssh/sshd_config
+sudo service ssh restart
